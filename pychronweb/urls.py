dac2acd559534beb991cf4c0b92fcb3fa605efea
Jake Ross <jirhiker@gmail.com>
2021-09-03 14:02:07 -0600

    added homepage

--- pychronweb/urls.py
+++ pychronweb/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from pychronweb import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('samples/', include('samples.urls')),
     path('projects/', include('projects.urls')),
