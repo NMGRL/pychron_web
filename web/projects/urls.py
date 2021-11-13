71e30e0a81e2937fcad96bb01a2ad9112a2d9076
Jake Ross <jirhiker@gmail.com>
2021-11-13 09:11:55 -0700

    fixed project cleanup reverse

--- web/projects/urls.py
+++ web/projects/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('submit_project', views.submit_project, name='submit_project'),
     path('entry', views.entry, name='entry'),
     path('<int:pk>/', views.ProjectDetailView.as_view(), name='detail'),
-    path('cleanup', views.cleanup, name='cleanup_projects'),
+    path('cleanup', views.cleanup, name='cleanup'),
 
 ]
 # ============= EOF =============================================
